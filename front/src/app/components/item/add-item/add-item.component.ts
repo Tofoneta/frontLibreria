c0a994238dd76a758b5d1037d86d5ea06ae3fff1
Tomás Tofoni <tomastofoni@gmail.com>
2023-05-30 18:36:32 -0300

    Update

--- front/src/app/components/item/add-item/add-item.component.ts
+++ front/src/app/components/item/add-item/add-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from 'src/app/services/item.service';
 import { item } from 'src/app/models/item';
-import { FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { libro } from 'src/app/models/libro';
 
 @Component({
@@ -10,59 +10,99 @@ import { libro } from 'src/app/models/libro';
   styleUrls: ['./add-item.component.css']
 })
 export class AddItemComponent implements OnInit {
-  isbn : any;
 
-  titulo: any;
-  autor: any;
-  area: any;
-  edicion: any;
-  paginas: any;
-  editorial: any;
-  precio: any;
 
+
+  itemN = new item();
+  libroN = new libro();
+  
+
+
+  itemForm: FormGroup;
+  libroForm: FormGroup;
   constructor(private ItemService : ItemService ) { }
-  item = new item();
-  libro = new libro();
 
+
+  
 
   ngOnInit(){
+
+    this.itemN.libro = this.libroN;
+    this.itemN.libro.isbn = 0;
+    this.itemN.libro.titulo = 'Hola';
+    this.itemN.libro.autor = '';
+    this.itemN.libro.area = '';
+    this.itemN.libro.numeroEdicion = 1;
+    this.itemN.libro.cantidadPaginas = 0;
+    this.itemN.libro.editorial = '';
+    this.itemN.precio = 3000;
+    alert(this.itemN.precio)
+    this.itemForm = new FormGroup({
+      'isbn' : new FormControl(this.itemN.libro.isbn,Validators.required),
+      'titulo': new FormControl(this.itemN.libro.titulo,Validators.required),
+      'autor': new FormControl(this.itemN.libro.autor,Validators.required),
+      'area': new FormControl(this.itemN.libro.area,Validators.required),
+      'numeroEdicion': new FormControl(this.itemN.libro.numeroEdicion,Validators.required),
+      'paginas': new FormControl(this.itemN.libro.cantidadPaginas,Validators.required),
+      'editorial': new FormControl(this.itemN.libro.editorial,Validators.required),
+      'precio': new FormControl(this.itemN.precio,Validators.required)
+
+
+      //'isbn' = new FormGro(this.item.libro.isbn.{valdators})
+
+    })
   
-    //oid    
+    
   }
 
-  addSucursal(){
-    /*
+  get isbn(){return this.itemForm.get('isbn')}
+  get titulo(){return this.itemForm.get('titulo')}
+  get autor(){return this.itemForm.get('autor')}
+  get area(){return this.itemForm.get('area')}
+  get numeroEdicion(){return this.itemForm.get('numeroEdicion')}
+  get paginas(){return this.itemForm.get('paginas')}
+  get editorial(){return this.itemForm.get('editorial')}
+  get precio(){return this.itemForm.get('precio')}
+  addItem(){
 
-    let Item = new item();
-    let Libro = new libro();
+    let ItemAux = new item();
+    let LibroAux = new libro();
+
     
-    Item.libro.isbn = this.isbn;
-    Item.libro.area = this.area;
-    Item.libro.titulo = this.titulo;
-    Item.libro.autor = this.autor;
-    Item.libro.editorial = this.editorial;
-    Item.libro.edicion = this.edicion;
-    Item.libro.cantidadPaginas = this.paginas;
-    Item.precio = this.precio;
-    */
-    //    this.sucursalService.getAll().subscribe(response => {
+   
+    LibroAux.area = this.area.value;
+    LibroAux.autor = this.autor.value;
+    LibroAux.cantidadPaginas = this.paginas.value;
+    LibroAux.editorial = this.editorial.value;
+    LibroAux.isbn = this.isbn.value;
+    LibroAux.numeroEdicion = this.numeroEdicion.value;
+    LibroAux.titulo = this.titulo.value;
+    ItemAux.precio = this.precio.value;
 
 
-    let Item = new item();
-    let Libro = new libro();
+  
+    ItemAux.libro = LibroAux;
+
+    alert(this.precio.value);
+
+    //// No se guardan los datos pasados por los válidos porqué ni siquiera los toma. Se guardan los datos de los get
+
+      
     
-    Libro.isbn = this.isbn;
-    Libro.area = this.area;
-    Libro.titulo = this.titulo;
-    Libro.autor = this.autor;
-    Libro.editorial = this.editorial;
-    Libro.edicion = this.edicion;
-    Libro.cantidadPaginas = this.paginas;
-    Item.precio = this.precio;
-    Item.libro = Libro;
+   
 
-
-    this.ItemService.add(Item).subscribe(response =>{
+    this.ItemService.add(ItemAux).subscribe(response =>{
+      
+      
+      this.area.setValue('')
+      this.autor.setValue('')
+      this.paginas.setValue('')
+      this.editorial.setValue('')
+      this.isbn.setValue('')
+      this.numeroEdicion.setValue('')
+      this.titulo.setValue('')
+      this.precio.setValue('')
+      console.log(ItemAux.precio)
 
       alert("Alta exitosa")     
       
@@ -73,7 +113,7 @@ export class AddItemComponent implements OnInit {
       }
     })
 
-
+  
 
     ///    this.ItemService.delete(id_item).subscribe(() => {
 
